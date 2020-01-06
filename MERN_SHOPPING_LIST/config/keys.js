7dc042554d85bfe3391a098b2c86c15de58e9b76
Debarshi-Choudhury <debarshi.choudhury1@gmail.com>
2020-01-07 03:05:25 +0530

    mern-blog ep 2

---
+++ MERN_SHOPPING_LIST/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb://user123:user123@ds131061.mlab.com:31061/mern_shopping'
+}
--- MERN_SHOPPING_LIST/server.js
+++ MERN_SHOPPING_LIST/server.js
@@ -2,11 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //Bodyparser middleware
 app.use(bodyParser.json());
 
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+.connect(db)
+.then(()=>console.log('mongodb connected...'))
+.catch(()=>console.log(err));
 
 
+//use routes
+app.use('/api/items', items);
 
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>console.log(`server started on port ${port}`));
