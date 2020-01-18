2390b5a0bcb9f502a140cea180fb3304131962c4
Debarshi-Choudhury <debarshi.choudhury1@gmail.com>
2020-01-18 20:00:55 +0530

    saving changes

--- MERN_SHOPPING_LIST/routes/api/items.js
+++ MERN_SHOPPING_LIST/routes/api/items.js
@@ -10,7 +10,26 @@ const Item = require('../../models/Item');
 router.get('/',(req,res)=>{
 	Item.find()
 	.sort({date:-1})
-	.then(item=>res.json(items))
+	.then(items=>res.json(items))
+})
+
+//@route POST api/items
+//@desc Create An Item
+//@access Public
+router.post('/',(req,res)=>{
+	const newItem = new Item({
+		name: req.body.name
+	});
+	newItem.save().then(item=>res.json(item));
+})
+
+//@route DELETE api/items/:id
+//@desc Delete An Item
+//@access Public
+router.delete('/:id',(req,res)=>{
+	Item.findById(req.params.id).then(item=>
+		item.remove().then(()=>res.json({success:true})))
+	.catch(err=>res.status(404).json({success:false}));
 })
 
 module.exports = router;
